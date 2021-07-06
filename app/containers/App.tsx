8491502fc3ffed1f12a34f273feb30e6f48bf9b0
José Luis Di Biase <josx@interorganic.com.ar>
2021-07-06 18:36:35 -0300

    1.1.4
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import { Typography } from '@material-ui/core';
 import { version } from '../constants/misc';
 import { ipcRenderer } from 'electron';
 
-const Version = () => (
+const Version = ({v1}) => (
   <Typography
     style={{
       position: 'absolute',
@@ -15,7 +15,7 @@ const Version = () => (
       cursor: 'default',
     }}
   >
-    {`v${version}`}
+    {`v${v1}`}
   </Typography>
 );
 
@@ -24,7 +24,8 @@ type Props = {
 };
 
 export default function App(props: Props) {
-  console.log(ipcRenderer,"--------------");
+  const [state, setState] = useState(false);
+  const [ver, setVer] = useState(version);
   const theme = createMuiTheme({
     palette: {
       common: {
@@ -62,11 +63,16 @@ export default function App(props: Props) {
     },
   });
 
+  useEffect(() => {
+    ipcRenderer.on('update_available', () => setState(true) );
+	return () => { ipcRenderer.removeAllListeners(['update_available']); };
+  }, []);
+
   const { children } = props;
   return (
     <ThemeProvider theme={theme}>
       {children}
-      <Version />
+      {!state ? <Version v1={version} /> : <Version v1="upgrade"/> }
     </ThemeProvider>
   );
 }
